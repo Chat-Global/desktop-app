492af511e5668cf0f68adba6c4acc2ca882973a0
tnfAngel <sniperhalfing@gmail.com>
2022-01-10 00:42:38 +0000

    Added source

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow } = require('electron');
+const { join } = require('path');
+
+let mainWindow = null;
+
+const iconPath = join(__dirname, (process.platform === 'win32') ? 'assets/icon.ico' : 'assets/icon.png');
+
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		frame: true,
+		backgroundColor: '#25262a',
+		title: 'Chat Global App',
+		icon: iconPath,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	});
+
+    //mainWindow.setMenu(null);
+	mainWindow.loadURL('https://app.chatglobal.ml');
+	console.log('Main window created.');
+
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+
+		console.log('Main window closed.');
+
+		app.quit();
+	});
+}
+
+app.whenReady().then(() => {
+	createMainWindow();
+
+	app.on('activate', () => {
+		console.log('Application activated.');
+
+		if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+	});
+});
+
+app.on('window-all-closed', () => {
+	console.log('All windows closed');
+
+	if (process.platform !== 'darwin') app.quit();
+});
